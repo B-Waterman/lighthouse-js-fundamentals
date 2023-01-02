c3d7263e39d4ad6e1f155df2167e58f53423fc1d
B-Waterman <brooke.waterman@outlook.com>
2023-01-02 14:55:05 -0800

    Use a for loop to repeat a chorus line

---
+++ chorus-for.js
@@ -0,0 +1,19 @@
+const chorus = "Let's dance!";
+for (let repeat = 0; repeat < 10; repeat++) {
+  //for loop requires three statements: initialize loop; define while cond.; update loop state
+  console.log(chorus);
+}
+console.log("Until the sun comes up!");
+/* prints the following to console:
+Let's dance!
+Let's dance!
+Let's dance!
+Let's dance!
+Let's dance!
+Let's dance!
+Let's dance!
+Let's dance!
+Let's dance!
+Let's dance!
+Until the sun comes up!
+*/
