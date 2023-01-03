d33eea361b4870a1425cabbe321cfabb38f95d1f
B-Waterman <brooke.waterman@outlook.com>
2023-01-03 03:01:06 -0800

    Testing arrays & accessing array elements

---
+++ ice-cream.js
@@ -0,0 +1,14 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+iceCreamFlavours.push("root beer");
+//adds "root beer" to the array iceCreamFlavours
+console.log(iceCreamFlavours);
+console.log(iceCreamFlavours[0]);
+//prints first flavour in list
+console.log(iceCreamFlavours[5]);
+//prints 6th/last flavour in list
+console.log(iceCreamFlavours.length);
+//prints total number of flavours in list
